68d42881ba3cb7141ba41e87fc1f5056e1a6f93c
nikhil8182 <nikhildeepak8182@gmail.com>
2023-02-13 07:48:43 +0530

    n

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask
-import os, keys
+import keys
 from twilio.rest import Client
 
 app = Flask(__name__)
 account_sid = keys.ssid
 auth_token = keys.token
 client = Client(account_sid, auth_token)
-templates = ['Hai, This is Onyx Onwords Smart Assistant. This is my whatsapp number reply hear to talk to me!',]
+templates = ['Hai, This is Onyx Onwords Smart Assistant. This is my whatsapp number reply hear to talk to me!', ]
 
 message = client.messages.create(
-                              body=str(templates[0]),
-                              from_='whatsapp:+18144812393',
-                              to='whatsapp:+919095640275'
-                          )
+    body=str(templates[0]),
+    from_='whatsapp:+18144812393',
+    to='whatsapp:+919095640275'
+)
 
 print(message.sid)
 
@@ -20,6 +20,7 @@ print(message.sid)
 @app.route("/")
 def hello():
     return "Hello, this is your WhatsApp API"
+
 
 if __name__ == "__main__":
     app.run()
